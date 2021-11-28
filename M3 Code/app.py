dac8746f5a720883244038fa5daff72f92080d61
nimuf <70873200+nimuf@users.noreply.github.com>
2021-11-28 17:37:05 +0800

    Update app.py

--- M3 Code/app.py
+++ M3 Code/app.py
@@ -1,24 +1,49 @@
-from flask import Flask, render_template
-from werkzeug.datastructures import RequestCacheControl
+from flask import Flask
+from datetime import datetime
+import re
+from flask import render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def main():
-    return render_template('lessonplans.html')
+@app.route("/")
+def home():
+    return render_template("home.html")
 
-@app.route('/lessonplan', methods=['GET', 'POST'])
-def setlessonplan():
-    with open('./static/data.json') as json_data:
-        lesson_data = json.load(json_data)
-    if request.method == 'POST':
-        lessonplan = request.form["lp"]
-        if lessonplan in lesson_data["lessonplanA"]:
-            return redirect(url_for("lessonplanA"))
-        elif lessonplan in lesson_data["lessonplanB"]:
-            return redirect(url_for("lessonplanB"))
-        elif lessonplan in lesson_data["lessonplanC"]:
-            return redirect(url_for("lessonplanC"))        
+# New functions
+@app.route("/validatepin/")
+def validatepin():
+    return render_template("validatepin.html")
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route("/scratcheasy/")
+def scratcheasy():
+    return render_template("scratcheasy.html")
+
+@app.route("/scratcheasy2/")
+def scratcheasy2():
+    return render_template("scratcheasy2.html")
+
+@app.route("/scratchmedium/")
+def scratchmed():
+    return render_template("scratchmedium.html")
+
+@app.route("/scratchhard/")
+def scratchhard():
+    return render_template("scratchhard.html")
+
+
+@app.route("/dashboard/")
+def dashboard():
+    return render_template("dashboard.html")
+
+@app.route("/hello/")
+@app.route("/hello/<name>")
+def hello_there(name = None):
+    return render_template(
+        "hello_there.html",
+        name=name,
+        date=datetime.now()
+    )
+
+@app.route("/api/data")
+def get_data():
+    return app.send_static_file("data.json")
